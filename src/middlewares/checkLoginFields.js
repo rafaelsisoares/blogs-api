4044ad4f656a9b266e7f324af027ddf242bea89f
Rafael Soares <r.soares2015@outlook.com>
2023-01-26 19:17:11 -0300

    fix: fix lint issues

--- src/middlewares/checkLoginFields.js
+++ src/middlewares/checkLoginFields.js
@@ -1,7 +1,8 @@
 module.exports = (req, res, next) => {
     const { email, password } = req.body;
 
-    if (!email || !password) return res.status(400).json({ message: 'Some required fields are missing' });
-
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }
     return next();
 };
