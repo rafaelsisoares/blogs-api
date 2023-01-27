8c0410752fc390727c8ebe7d0b0f57d6d97b35cc
Rafael Soares <r.soares2015@outlook.com>
2023-01-26 22:07:23 -0300

    fix: Correcao nos valores a serem validados

--- src/services/validators/validateNewUserData.js
+++ src/services/validators/validateNewUserData.js
@@ -1,7 +1,8 @@
 const { newUserSchema } = require('./schemas');
 
 module.exports = (newUser) => {
-    const { error } = newUserSchema.validate(newUser);
+    const { displayName, email, password } = newUser;
+    const { error } = newUserSchema.validate({ displayName, email, password });
 
     if (error) return { type: 'BAD_REQUEST', message: error.message };
 
