998150a8c45ffb31f74cccdde8d11debb6df065f
Rafael Soares <r.soares2015@outlook.com>
2023-01-27 16:37:02 -0300

    feat: Criacao do model Category

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        timestamps: false,
+        tableName: 'categories',
+        underscored: true,
+    });
+
+    return Category;
+};
