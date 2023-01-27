4fd07b3199bcf1c0d53e039fe50038035f248c9b
Rafael Soares <r.soares2015@outlook.com>
2023-01-26 23:21:05 -0300

    chore: Criacao do middleware de erro

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,6 +1,6 @@
 module.exports = (error, _req, res, _next) => {
     if (error.statusCode) {
-        return res.status(error.statusCode).json(error.message);
+        return res.status(error.statusCode).json({ message: error.message });
     }
 
     console.error(error.message);
