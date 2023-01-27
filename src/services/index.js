1b748398a02e09b4375b323ed915117e2be8914d
Rafael Soares <r.soares2015@outlook.com>
2023-01-27 17:07:11 -0300

    chore: Criacao do service de Category

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const loginService = require('./Login');
 const userService = require('./User');
+const categoryService = require('./Category');
 
 module.exports = {
     loginService,
     userService,
+    categoryService,
 };
