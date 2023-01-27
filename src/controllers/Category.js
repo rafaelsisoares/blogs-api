171687e7a629e524fd9d03f90b6ba90d746c26f6
Rafael Soares <r.soares2015@outlook.com>
2023-01-27 19:10:14 -0300

    chore: Criacao da funcao getAllCategories

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -11,6 +11,17 @@ const createCategory = async (req, res) => {
     }
 };
 
+const getAllCategories = async (_req, res) => {
+    try {
+        const categories = await categoryService.getAllCategories();
+        res.status(200).json(categories);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 module.exports = {
     createCategory,
+    getAllCategories,
 };
