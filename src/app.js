e50824f9003b8236fd957c997d570d415b4423ed
Rafael Soares <r.soares2015@outlook.com>
2023-01-26 23:31:03 -0300

    feat: Criacao da rota GET /users

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const loginRouter = require('./routes/Login.Router');
 const userRouter = require('./routes/User.Router');
+const errorMiddleware = require('./middlewares/error');
 
 // ...
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use('/login', loginRouter);
 app.use('/user', userRouter);
+app.use(errorMiddleware);
 
 // ...
 
--- src/routes/User.Router.js
+++ src/routes/User.Router.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const { userController } = require('../controllers');
+const checkToken = require('../middlewares/checkToken');
 
 const router = Router();
 
+router.get('/', checkToken, userController.getAllUsers);
 router.post('/', userController.createUser);
 
 module.exports = router;
