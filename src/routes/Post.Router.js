9e9f82fa24d53d56ac7f4c6b6cc21290e03f36d4
Rafael Soares <r.soares2015@outlook.com>
2023-01-30 17:34:37 -0300

    feat: Criacao da rota PUT /post/:id

--- src/routes/Post.Router.js
+++ src/routes/Post.Router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 
 const { postController } = require('../controllers');
 const checkNewPostFields = require('../middlewares/checkNewPostFields');
+const checkEditPostFields = require('../middlewares/checkEditPostFields');
 const checkToken = require('../middlewares/checkToken');
 
 const router = Router();
@@ -9,5 +10,6 @@ const router = Router();
 router.post('/', checkToken, checkNewPostFields, postController.createPost);
 router.get('/', checkToken, postController.getAllPosts);
 router.get('/:id', checkToken, postController.getPostById);
+router.put('/:id', checkToken, checkEditPostFields, postController.editPost);
 
 module.exports = router;
